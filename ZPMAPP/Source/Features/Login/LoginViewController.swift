812c5c3bb5c923009916f2a99c67bb383ab874dd
Alan <allanpereir@gmail.com>
2021-08-24 21:04:05 -0300

    Inserido as constraints na tela de Login e adicionado os ícones nos textfield.

--- ZPMAPP/Source/Features/Login/LoginViewController.swift
+++ ZPMAPP/Source/Features/Login/LoginViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var btnUser: UIButton!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtUser: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let emailImage = UIImage(systemName: "mail")
+        addLeftImageTo(txtField: txtUser, andImage: emailImage!)
+        
+    
     }
-
-
+    
+    func addLeftImageTo(txtField: UITextField, andImage img: UIImage){
+        let leftImageView = UIImageView(frame: CGRect(x: 0.0, y:0.0, width: img.size.width, height: img.size.height))
+        
+        leftImageView.image = img
+        txtField.leftView = leftImageView
+        txtField.leftViewMode = .always
+        
+    }
+    
 }
