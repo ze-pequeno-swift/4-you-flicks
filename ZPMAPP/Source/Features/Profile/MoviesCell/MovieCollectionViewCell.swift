70530dc6a30f3afbc2beafc016af0635b6da887a
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-08-31 06:52:41 -0300

    Create MovieCollectionViewCell

---
+++ ZPMAPP/Source/Features/Profile/MoviesCell/MovieCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieCollectionViewCell.swift
+//  ZPMAPP
+//
+//  Created by Felipe Rocha Oliveira on 31/08/21.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
