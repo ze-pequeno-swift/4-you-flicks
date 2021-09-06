73fbcfd8767fb2d13899842f0611388009068a43
Alan Silva <allanpereir@gmail.com>
2021-09-06 00:47:30 -0300

    Created NewCustomCell.

---
+++ ZPMAPP/Source/Features/Home/View/Cells/NewCustomCell/TopCustomCell.swift
@@ -0,0 +1,46 @@
+//
+//  TopCustomCell.swift
+//  ZPMAPP
+//
+//  Created by Alan Silva on 06/09/21.
+//
+
+import UIKit
+
+class TopCustomCell: UITableViewCell {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    static var identifier: String {
+        String(describing: TopCustomCell.self)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        CustomCollectionCell.registerOn(collectionView)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+
+}
+extension TopCustomCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = CustomCollectionCell.identifier
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+                as? CustomCollectionCell else { return UICollectionViewCell() }
+        
+        cell.setupMovieCell()
+        
+        return cell
+    }
+}
+
