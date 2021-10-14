c7379337a876f09c0bd024ba68233ea1abbbc5bb
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-10-14 12:27:46 -0300

    Updated profile view controller, add controller and delegates

--- ZPMAPP/Source/Features/Profile/View/ProfileViewController.swift
+++ ZPMAPP/Source/Features/Profile/View/ProfileViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     @IBOutlet weak var profileTableView: UITableView!
+    
+    var controller: ProfileController!
 
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.controller = ProfileController()
+        self.controller.delegate = self
+
         self.setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        showLoading()
+        self.controller.getCustomerInfo()
+        self.profileTableView.reloadData()
+        hideLoading()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let optionVC: OptionsViewController? = segue.destination as? OptionsViewController
+        let customer: Customer? = sender as? Customer
+        optionVC?.controller = OptionsController(customer: customer)
     }
     
     // MARK: - Private functions
@@ -28,34 +47,63 @@ class ProfileViewController: UIViewController {
         ListMoviesTableViewCell.registerOn(profileTableView)
     }
     
-    func getHeaderCell() -> UITableViewCell {
+    private func getHeaderCell() -> UITableViewCell {
         let identifier = HeaderProfileTableViewCell.identifier
         let cell = profileTableView.dequeueReusableCell(withIdentifier: identifier) as? HeaderProfileTableViewCell
 
         cell?.delegate = self
+        cell?.setup(customer: self.controller.getCustomer())
 
         return cell ?? UITableViewCell()
     }
     
-    func getFilterCell() -> UITableViewCell {
+    private func getFilterCell() -> UITableViewCell {
         let identifier = FilterTableViewCell.identifier
-        let cell = profileTableView.dequeueReusableCell(withIdentifier: identifier) as? FilterTableViewCell ?? UITableViewCell()
+        let cell = profileTableView.dequeueReusableCell(withIdentifier: identifier) as? FilterTableViewCell
         
-        return cell
+        let contWatch = self.controller.countWatches(tag: .watch)
+        let contWatched = self.controller.countWatches(tag: .watched)
+        let contWatching = self.controller.countWatches(tag: .watching)
+        
+        cell?.delegate = self
+        cell?.setup(watch: contWatch, watched: contWatched, watching: contWatching)
+        
+        return cell ?? UITableViewCell()
     }
     
-    func getFollowersCell() -> UITableViewCell {
+    private func getFollowersCell() -> UITableViewCell {
         let identifier = FollowersTableViewCell.identifier
-        let cell = profileTableView.dequeueReusableCell(withIdentifier: identifier) as? FollowersTableViewCell ?? UITableViewCell()
+        let cell = profileTableView.dequeueReusableCell(withIdentifier: identifier) as? FollowersTableViewCell
         
-        return cell
+        let contFollowers = self.controller.countFollowers()
+        let contFollowings = self.controller.countFollowings()
+        cell?.setup(following: contFollowings, followers: contFollowers)
+        
+        return cell ?? UITableViewCell()
     }
     
-    func getMovieCell() -> UITableViewCell {
+    private func getMovieCell() -> UITableViewCell {
         let identifier = ListMoviesTableViewCell.identifier
-        let cell = profileTableView.dequeueReusableCell(withIdentifier: identifier) as? ListMoviesTableViewCell ?? UITableViewCell()
+        let cell = profileTableView.dequeueReusableCell(withIdentifier: identifier) as? ListMoviesTableViewCell
         
-        return cell
+        cell?.delegate = self
+        cell?.setup(
+            customer: self.controller.getCustomer(),
+            myMovies: self.controller.getMyMovies()
+        )
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    private func proceedToMovieNav() {
+        let homeController = UIStoryboard(name: "Home", bundle: nil)
+        guard let viewController = homeController.instantiateViewController(identifier: String(describing: MovieDetailsViewController.self)) as? MovieDetailsViewController else { return }
+        
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func executePerformSegue(identifier: String) {
+        self.performSegue(withIdentifier: identifier, sender: self.controller.getCustomer())
     }
 }
 
@@ -93,8 +141,21 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
-extension ProfileViewController: HeaderProfileProtocol {
+extension ProfileViewController: HeaderProfileProtocol, ProfileControllerProtocol, ListMoviesTableViewCellProtocol, FilterTableViewCellProtocol {
+    func filterMovie(tag: Tag) {
+        self.controller.filterMovie(tag: tag)
+        self.profileTableView.reloadData()
+    }
+    
+    func proceedToMovie() {
+        self.proceedToMovieNav()
+    }
+    
+    func reloadTableView() {
+        self.profileTableView.reloadData()
+    }
+    
     func tappedPerformSegue(identifier: String) {
-        self.performSegue(withIdentifier: identifier, sender: nil)
+        self.executePerformSegue(identifier: identifier)
     }
 }
