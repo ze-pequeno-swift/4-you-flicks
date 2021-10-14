4b0728507309279bcd4caccd1b4f53a3c15288f6
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-10-14 12:37:17 -0300

    create extension valid fields and animation error

--- ZPMAPP/Source/Features/Shared/Extensions/UITextField+Extensions.swift
+++ ZPMAPP/Source/Features/Shared/Extensions/UITextField+Extensions.swift
@@ -39,4 +39,47 @@ extension UITextField{
     @objc func doneButtonAction() {
         self.resignFirstResponder()
     }
+    
+    func shakeAnimation() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        animation.duration = 0.6
+        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+        layer.add(animation, forKey: "shake")
+    }
+    
+    func isEmail() -> Bool {
+        let emailRegEx = "^.+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*$"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self.text)
+    }
+    
+    func isEmpty() -> Bool {
+        return self.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+    }
+    
+    func invalidField() {
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.red.cgColor
+        self.textColor = .red
+    }
+    
+    func validField() {
+        self.textColor = .black
+        self.layer.borderWidth = 0
+    }
+    
+    func min(qty: Int) -> Bool {
+        if let text = self.text {
+            return text.count >= qty
+        }
+        
+        return false
+    }
+    
+    func isPasswordValid() -> Bool {
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: self.text)
+    }
 }
