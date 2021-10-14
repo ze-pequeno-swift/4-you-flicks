75df160dc59ae5fec43201f55cee0505f65b8835
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-10-14 12:34:29 -0300

    Add customer and movies in list movies collection view

--- ZPMAPP/Source/Features/Profile/View/CustomCell/MoviesSection/ListMoviesTableViewCell.swift
+++ ZPMAPP/Source/Features/Profile/View/CustomCell/MoviesSection/ListMoviesTableViewCell.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
+protocol ListMoviesTableViewCellProtocol: AnyObject {
+    func proceedToMovie()
+}
+
 class ListMoviesTableViewCell: UITableViewCell {
     
     static let identifier: String = String(describing: ListMoviesTableViewCell.self)
+    private var customer: Customer?
+    private var myMovie: [MyMovie]? = []
+    
+    var delegate: ListMoviesTableViewCellProtocol?
     
     // MARK: - CollectionView
 
@@ -27,17 +35,15 @@ class ListMoviesTableViewCell: UITableViewCell {
         
         MovieCollectionViewCell.registerOn(self.moviesCollectionView)
     }
+    
+    func setup(customer: Customer?, myMovies: [MyMovie]?) {
+        self.myMovie = myMovies
+        self.customer = customer
+        moviesCollectionView.reloadData()
+    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-    }
-    
-    private func proceedToMovie() {
-        let homeController = UIStoryboard(name: "Home", bundle: nil)
-        guard let viewController = homeController.instantiateViewController(identifier: "MovieDetailsViewController")
-                as? MovieDetailsViewController else { return }
-        
-        //self.navigationController?.pushViewController(viewController, animated: true)
     }
     
 }
@@ -45,17 +51,23 @@ class ListMoviesTableViewCell: UITableViewCell {
 // MARK: - UICollectionViewDelegate, UICollectionViewDataSource
 extension ListMoviesTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return self.myMovie?.count ?? 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = MovieCollectionViewCell.identifier
+        let movieCell = moviesCollectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? MovieCollectionViewCell
         
-        let movieCell: MovieCollectionViewCell? = moviesCollectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as? MovieCollectionViewCell
+        if let movie = self.myMovie?[indexPath.row].movie,
+           let tag = self.myMovie?[indexPath.row].tag,
+           let customer = self.customer {
+            movieCell?.setup(customer: customer, movie: movie, tag: tag)
+        }
         
         return movieCell ?? UICollectionViewCell()
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.proceedToMovie()
+        self.delegate?.proceedToMovie()
     }
 }
