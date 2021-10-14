0c73b4950c4fbde932cfe458db47db3a0fd5494b
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-10-14 12:20:43 -0300

    create reset controller, add firebase and delegate

--- ZPMAPP/Source/Features/Profile/Controller/ResetController.swift
+++ ZPMAPP/Source/Features/Profile/Controller/ResetController.swift
@@ -7,6 +7,58 @@
 
 import Foundation
 
+protocol ResetControllerProtocol: AnyObject {
+    func sendRecoveryPassword()
+    func sendErrorRecoveryPassword()
+}
+
 class ResetController {
+
+    var customer: Customer?
+    let firebase: FirebaseDataService!
     
+    weak var delegate: ResetControllerProtocol?
+    
+    init(customer: Customer?) {
+        self.customer = customer
+        self.firebase = FirebaseDataService()
+        self.firebase.delegate = self
+        self.firebase.delegateRecovery = self
+    }
+    
+    func getCustomer() -> Customer? {
+        return self.customer
+    }
+    
+    func setCustomer() {
+        self.customer = self.firebase.loadCustomer()
+    }
+    
+    func resetPassword(password: String, oldPassword: String) {
+        self.firebase.resetPassword(password: password)
+    }
+    
+    func recoveryPassword() {
+        self.firebase.recoveryPassword()
+    }
+}
+
+extension ResetController: FirebaseDataServiceProtocol, FirebaseRecoveryPasswordProtocol {
+    func success(_ collection: String) {
+        if collection == "users" {
+            self.setCustomer()
+        }
+    }
+    
+    func failure(error: Error?) {
+        print(error)
+    }
+    
+    func recovery() {
+        self.delegate?.sendRecoveryPassword()
+    }
+    
+    func errorRecovery(error: Error?) {
+        print("error")
+    }
 }
