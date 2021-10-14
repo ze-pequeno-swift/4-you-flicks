b2810a0bb706a4417ccdaac6949a01dceca0c611
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-10-14 12:19:30 -0300

    Create options controller, add firebase and delegate

--- ZPMAPP/Source/Features/Profile/Controller/OptionsController.swift
+++ ZPMAPP/Source/Features/Profile/Controller/OptionsController.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 class OptionsController {
+    
+    var customer: Customer?
+    let firebase: FirebaseDataService!
+    
+    init(customer: Customer?) {
+        self.customer = customer
+        self.firebase = FirebaseDataService()
+        self.firebase.delegate = self
+    }
+    
+    func getCustomer() -> Customer? {
+        return self.customer
+    }
 
+}
+
+extension OptionsController: FirebaseDataServiceProtocol {
+    func success(_ collection: String) {
+        print("OK Option")
+    }
+    
+    func failure(error: Error?) {
+        print("error")
+    }
 }
